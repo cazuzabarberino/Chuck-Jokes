edf0d403dd1c7f48f9192c0e4565e2813017122f
cazuzaneves <cazuzaneves@gmail.com>
2020-06-27 11:52:03 -0300

    Updates

---
+++ precache-manifest.2d5292250b8398cd24610b16fa9b8770.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df90d06ca5c066d5d151cc9f9085e42",
+    "url": "/Chuck-Jokes/index.html"
+  },
+  {
+    "revision": "42f5b64e85a46373e32c",
+    "url": "/Chuck-Jokes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6c4d44b152722538ff7e",
+    "url": "/Chuck-Jokes/static/js/2.9352dcac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Chuck-Jokes/static/js/2.9352dcac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42f5b64e85a46373e32c",
+    "url": "/Chuck-Jokes/static/js/main.5f152fd3.chunk.js"
+  },
+  {
+    "revision": "1ca91e40fe73215d9151",
+    "url": "/Chuck-Jokes/static/js/runtime-main.da0be3c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Chuck-Jokes/static/media/logo.5d5d9eef.svg"
+  }
+]);
